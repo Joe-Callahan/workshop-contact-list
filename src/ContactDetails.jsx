ec7abf4b544f5ae16db5d44ec357495e42fea95e
Joe Callahan <joecallahan@macbookair.lan>
2025-02-04 14:18:33 -0800

    added contact detail rendering and a back button

--- src/ContactDetails.jsx
+++ src/ContactDetails.jsx
@@ -1,5 +1,16 @@
-const ContactDetails = () => {
-  return <h1>Contact Details</h1>;
+const ContactDetails = (props) => {
+  return (
+    <>
+      <h2>{props.selectedContact.name}</h2>
+      <p>Phone: {props.selectedContact.phone}</p>
+      <p>Email: {props.selectedContact.email}</p>
+      <p>Address: {props.selectedContact.address.street}{", "}
+        {props.selectedContact.address.suite}</p>
+      <p>{props.selectedContact.address.city}{" "}
+      {props.selectedContact.address.zipcode}</p>
+      <button onClick={() => {props.setSelectedContact({}) }}>Back</button>
+    </>
+  )
 }
 
 export default ContactDetails
